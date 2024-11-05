c1824711586b9848702497a1e11a25116af800f4
Giuliana <gcastillo@safareyes.es>
2024-11-05 12:59:08 +0100

    fd

--- PHP/Clase/ya entregados/EJERCICIOS GC/sesiones/carrito.php
+++ PHP/Clase/ya entregados/EJERCICIOS GC/sesiones/carrito.php
@@ -10,7 +10,6 @@ $peliculas = [
 $reservas = $_SESSION['reservas'] ?? [];
 $carrito = $_SESSION['carrito'] ?? [];
 
-
 if (!isset($_SESSION['carrito_timer'])) {
     $_SESSION['carrito_timer'] = time();
 } elseif (time() - $_SESSION['carrito_timer'] > 6000) {
@@ -47,10 +46,6 @@ if (isset($_POST['finalizar_compra'])) {
         if (!in_array($asiento, $reservas[$pelicula_id][$horario])) {
             $reservas[$pelicula_id][$horario][] = $asiento;
             $asientos_txt .= "Película: {$peliculas[$pelicula_id]['nombre']}, Asiento: $asiento, Horario: $horario\n";
-        } else {
-            echo "<p>Error: El asiento $asiento en el horario $horario ya está reservado para la película {$peliculas[$pelicula_id]['nombre']}.</p>";
-            echo "<a href='index.php'>Volver a la tienda</a>";
-            exit();
         }
     }
 
@@ -58,10 +53,9 @@ if (isset($_POST['finalizar_compra'])) {
     unset($_SESSION['carrito']);
     unset($_SESSION['carrito_timer']);
 
-    file_put_contents('asientos_reservados.txt', $asientos_txt);
-
-    echo "<p>Compra realizada con éxito. Gracias por su compra.</p>";
-    echo "<a href='index.php'>Volver a la tienda</a>";
+    header('Content-Type: text/plain');
+    header('Content-Disposition: attachment; filename="entradas.txt"');
+    echo $asientos_txt;
     exit();
 }
 ?>
@@ -76,33 +70,22 @@ if (isset($_POST['finalizar_compra'])) {
 
 <body>
     <h2>Carrito de Compras</h2>
-    <?php if (!empty($carrito)): ?>
-        <table border="1">
-            <thead>
-                <tr>
-                    <th>Película</th>
-                    <th>Asiento (Fila-Asiento)</th>
-                    <th>Horario</th>
-                    <th>Precio (€)</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php foreach ($carrito as $entrada): ?>
-                    <tr>
-                        <td><?= $peliculas[$entrada['pelicula_id']]['nombre']; ?></td>
-                        <td><?= $entrada['asiento']; ?></td>
-                        <td><?= $entrada['horario']; ?></td>
-                        <td><?= $peliculas[$entrada['pelicula_id']]['precio']; ?>€</td>
-                    </tr>
-                <?php endforeach; ?>
-            </tbody>
-        </table>
-        <form method="post">
-            <button type="submit" name="finalizar_compra">Finalizar Compra</button>
-            <button type="submit" name="vaciar_carrito">Vaciar Carrito</button>
-        </form>
-    <?php else: ?>
+    <?php if (empty($carrito)): ?>
         <p>El carrito está vacío.</p>
+    <?php else: ?>
+        <ul>
+            <?php foreach ($carrito as $entrada): ?>
+                <li>
+                    Película: <?= $peliculas[$entrada['pelicula_id']]['nombre']; ?>,
+                    Asiento: <?= $entrada['asiento']; ?>,
+                    Horario: <?= $entrada['horario']; ?>
+                </li>
+            <?php endforeach; ?>
+        </ul>
+        <form method="post" action="">
+            <button type="submit" name="vaciar_carrito">Vaciar Carrito</button>
+            <button type="submit" name="finalizar_compra">Finalizar Compra</button>
+        </form>
     <?php endif; ?>
     <a href="index.php">Volver a la tienda</a>
 </body>
